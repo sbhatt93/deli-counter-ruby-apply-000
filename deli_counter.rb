cd6438ecee96b16def9cc74c241fca394d13f45c
jocular-inspector-4517 <shivbhatt@hotmail.com>
2019-02-26 17:51:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,9 +6,9 @@ def line (current_position)
   if current_position.length == 0
     puts "The line is currently empty."
   else
-    current_line = "The line is currently: "
+    current_line = "The line is currently:"
     current_position.each_with_index do |name,index|
-    current_line << "#{index+1}. #{name} "
+    current_line << " #{index+1}. #{name} "
    end
    puts current_line
   end
